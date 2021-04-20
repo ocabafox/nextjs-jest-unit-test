bcb802fecfd39343ee18e800cd0be636daf55d69
ocabafox <ocabafox@gmail.com>
2021-04-21 03:15:37 +0800

    add test that make sure the data is not change

--- pages/api/user/put/put.test.js
+++ pages/api/user/put/put.test.js
@@ -60,4 +60,30 @@ describe('GET /api/put/user/[id]', () => {
       gender: 'male',
     });
   });
+
+  test('Should return a user named "Luke Skywalker" with ID 1', async () => {
+    const { req, res } = createMocks({
+      method: 'GET',
+      query: {
+        id: '1',
+      },
+    });
+
+    await GetUserById(req, res);
+
+    const userResponse = JSON.parse(res._getData());
+    const users = fp.omit(['created_at', 'updated_at'])(userResponse);
+
+    expect(res._getStatusCode()).toBe(200);
+    expect(users).toEqual({
+      eye_color: 'blue',
+      gender: 'male',
+      hair_color: 'blond',
+      height: '172',
+      id: 1,
+      mass: '77',
+      name: 'Luke Skywalker',
+      skin_color: 'fair'
+    });
+  });
 });
